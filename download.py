bdd26659b51a2a2e07dbb3efdeae3473ad90bfa8
supertramp-sid <sid.siddhant.loya@gmail.com>
2016-06-19 01:04:12 +0530

    foo bar

--- download.py
+++ download.py
@@ -5,25 +5,26 @@ import threading
 # Obtain the link through 1. LocalDB 2. Scraping ######
 
 download_link = ""
+headers = ""
 
 # HEAD REQUEST TO KNOW THE SIZE OF THE FILE TO BE DOWNLOADED. #######
 
-head_response = requests.head(download_link).headers
 
-content_length = head_response[
-    'Content-length'] if head_response['Content-length'] else None
+download_status = {}
 
-
-# CODE TO SPAWN THREADS AND START DOWNLOADING #####
 
 class myThread(threading.Thread):
 	"""docstring for myThread"""
-	def __init__(self, **kwargs):
-		super(myThread, self).__init__()
+        global url
+        
+	def __init__(self, threadId, drange):
 		self.content_length = kwargs['length']
-		self.number_of_threads = kwargs['threads'] 
-	
-	def download(self):
+	        self.url = url
+                self.drange = drange
+	        super(myThread, self).__init__()
+                
+	def run(self):
+                download(self.threadId, self.drange, self.url)
 		""" code to download 
 			1. Setting headers.
 			2. Spawning threads.
@@ -34,5 +35,52 @@ class myThread(threading.Thread):
 			7. Merge the threads.
 			8. Save the search)if new) to database.
 			"""
+                
 		pass
 
+
+
+class DownloadFile(object):
+        def __init__(self,**kwargs):
+                self.url = kwargs['url']
+                self.count = kwargs['count']
+                self.size = None
+                self.threads = []
+                
+        def find_headers(self):
+                global headers = requests.head(download_link).headers
+                self.size = head_response['Content-length'] if head_response['Content-length'] else None
+            
+                
+        def add_to_queue(self):
+                threads = []
+                partition = (self.size)/4
+                for i in xrange(4):
+                        thread = myThread(i, [i*partition, (i+1)*partition])
+                        threads.append(thread)
+
+        def start_download(self):
+                for thread in self.threads:
+                        thread.start()
+
+        def wait_for_complete(self):
+                for thread in self.threads:
+                        thread.join()
+                
+        
+def download(threadId, drange, url):
+        headers = {"Range":"{0}-{1}".format(drange[0], drange[1])}
+        size = drange[1] - drange[0]
+        req = requests.get(url, headers=headers, stream=True)
+        download_status[size] = size
+        download_status[threadId] = 0
+        for data in req.iter_content():
+                download_status[threadId] += len(req)
+                # The download status must be updated in the global variable
+                # and must be shown by kivy in the form of a progress bar.
+
+####  TODO  ####
+
+# 1. Handling Exceptions.
+# 2. Error logging.
+# 3. Merging the files
